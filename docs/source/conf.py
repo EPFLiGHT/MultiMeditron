43777f76f06030ff3213b676cb4a392c12247668
MichelDucartier <m.zhang2490@gmail.com>
2025-10-05 21:45:25 +0000

    Solve deps for autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,8 @@ extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_tabs.tabs", "
 templates_path = ['_templates']
 exclude_patterns = []
 
+autodoc_mock_imports = ["torch", "flash-attn"]
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
